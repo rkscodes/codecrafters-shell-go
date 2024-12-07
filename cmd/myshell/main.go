07c9bf76a00a73108ceed912522feab718335ed5
Ram Krishna Singh <rkscodes@gmail.com>
2024-12-07 21:41:48 +0530

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
@@ -16,12 +17,40 @@ func main() {
 
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
-		var input, _ = bufio.NewReader(os.Stdin).ReadString('\n')
-		// Test if we need to exit
-		if input == "exit 0\n" {
-			return
+		var input, error = bufio.NewReader(os.Stdin).ReadString('\n')
+		if error != nil {
+			fmt.Fprint(os.Stderr, "Failed to read from stdin")
 		}
-		fmt.Fprint(os.Stdout, input[:len(input)-1]+": command not found\n")
+		// Seprate frist part of the comman and rest
+		parts := strings.SplitN(input, " ", 2)
+		command := parts[0]
+		args := ""
+		if len(parts) > 1 {
+			args = parts[1]
+		}
+		command = strings.TrimSuffix(strings.TrimSpace(command), "\n")
+		args = strings.TrimSuffix(strings.TrimSpace(args), "\n")
+		execute_command(command, args)
+	}
+
+}
+
+func execute_command(command string, args string) {
+	// fmt.Fprint(os.Stdout, command)
+	switch command {
+	case "echo":
+		fmt.Fprint(os.Stdout, args+"\n")
+		break
+
+	case "exit":
+		if len(args) > 0 && args == "0" {
+			os.Exit(0)
+		}
+		break
+
+	default:
+		fmt.Fprint(os.Stdout, command+": command not found\n")
+
 	}
 
 }
